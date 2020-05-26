d0f3e322498f2395af03a0a7de894f65eb76af63
zion830 <zion830@honglab.org>
2020-05-26 21:55:48 +0900

    TimeRangePickerDialog 사용하는 샘플 앱 구현

--- app/src/main/java/zion830/com/rangetimepickerdialog/MainActivity.kt
+++ app/src/main/java/zion830/com/rangetimepickerdialog/MainActivity.kt
@@ -1,12 +1,31 @@
 package zion830.com.rangetimepickerdialog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import zion830.com.range_picker_dialog.DateTimeUtil
+import zion830.com.range_picker_dialog.OnTimeRangeSelectedListener
+import zion830.com.range_picker_dialog.TimeRangePickerDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_selected_range.text = DateTimeUtil.getCurrentTimeRangeString()
+        btn_show_dialog.text = DateTimeUtil.getTodayString()
+        btn_show_dialog.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        TimeRangePickerDialog.newInstance(object : OnTimeRangeSelectedListener {
+            override fun onTimeSelected(startHour: Int, startMin: Int, endHour: Int, endMin: Int) {
+                tv_selected_range.text =
+                    DateTimeUtil.getReadableTimeString(startHour, endHour, startMin, endMin)
+            }
+        }).show(supportFragmentManager)
     }
 }
