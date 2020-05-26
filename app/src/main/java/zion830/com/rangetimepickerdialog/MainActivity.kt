60c20607b3d81a67f40d4afe52f71aeb035942e5
zion830 <zion830@honglab.org>
2020-05-26 20:43:28 +0900

    프로젝트 초기화

---
+++ app/src/main/java/zion830/com/rangetimepickerdialog/MainActivity.kt
@@ -0,0 +1,12 @@
+package zion830.com.rangetimepickerdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
