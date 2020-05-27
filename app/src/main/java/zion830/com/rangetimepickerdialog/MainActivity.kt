4a4c9da13b924ac939c92700a65c0e45cbc04b84
zion830 <zion830@honglab.org>
2020-05-27 18:24:13 +0900

    Change sample app example

--- app/src/main/java/zion830/com/rangetimepickerdialog/MainActivity.kt
+++ app/src/main/java/zion830/com/rangetimepickerdialog/MainActivity.kt
@@ -3,8 +3,6 @@ package zion830.com.rangetimepickerdialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import zion830.com.range_picker_dialog.OnTimeRangeSelectedListener
-import zion830.com.range_picker_dialog.TimeRange
 import zion830.com.range_picker_dialog.TimeRangePickerDialog
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_selected_range.text = DateTimeUtil.getCurrentTimeRangeString()
-        btn_show_dialog.text = DateTimeUtil.getTodayString()
         btn_show_dialog.setOnClickListener {
             showDialog()
         }
     }
 
     private fun showDialog() {
-        TimeRangePickerDialog.create(object : OnTimeRangeSelectedListener {
-            override fun onTimeSelected(timeRange: TimeRange) {
-                tv_selected_range.text = timeRange.getReadableTimeRange()
-            }
-        }).show(supportFragmentManager)
+        TimeRangePickerDialog.Builder()
+            .setTimeRange(10, 20, 16, 40)
+            .setTimeInterval(20)
+            .setOnTimeRangeSelectedListener { tv_selected_range.text = it.readableTimeRange }
+            .build()
+            .show(supportFragmentManager)
     }
 }
